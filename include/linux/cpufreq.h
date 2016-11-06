3d68b777522dbeb894b430e36e48857b048cc1e7
MEGAX91 <blackdeath1370@gmail.com>
2016-11-06 12:31:02 +0530

    cpufreq: Add Ironactive governor
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -536,6 +536,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
