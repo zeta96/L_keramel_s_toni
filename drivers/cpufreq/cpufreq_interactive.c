44eef00bb9afbc7b36a4f8a49fd16e410c86a933
gaurav jindal <gauravjindal1104@gmail.com>
2017-09-08 00:07:43 +0530

    drivers: cpufreq: checks to avoid kernel crash in cpufreq_interactive
    In cpufreq_governor_interactive, driver throws warning with WARN_ON
    for !tunables and event != CPUFREQ_GOV_POLICY_INIT.
    In case when tunables is NULL for event other than
    CPUFREQ_GOV_POLICY_INIT, kernel will crash as there is no safe check
    available before accessing tunables. So to handle such case and avoid
    the kernel crash, return -EINVAL if WARN_ON returns TRUE.
    
    [msfjarvis: Change existing BUG_ON to WARN_ON]
    Change-Id: I7a3a22d58e3c8a315a1cc1d31143649dc8807dee
    Signed-off-by: gaurav jindal <gauravjindal1104@gmail.com>
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1699,7 +1699,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 	else
 		tunables = common_tunables;
 
-	BUG_ON(!tunables && (event != CPUFREQ_GOV_POLICY_INIT));
+	if (WARN_ON(!tunables && (event != CPUFREQ_GOV_POLICY_INIT)))
+		return -EINVAL;
 
 	switch (event) {
 	case CPUFREQ_GOV_POLICY_INIT:
