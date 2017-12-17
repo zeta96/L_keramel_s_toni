63c0f7f3159a2ecfb7986f6039720927cc7e9aff
Ryan Andri <ryanandri@linuxmail.org>
2017-12-17 19:37:14 +0700

    cpufreq: add latest alucard/darkness/nightmare governors
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -527,6 +527,15 @@ extern struct cpufreq_governor cpufreq_gov_gabriel;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GABRIELPLUS)
 extern struct cpufreq_governor cpufreq_gov_gabrielplus;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_gabrielplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
