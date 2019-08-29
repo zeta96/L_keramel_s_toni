7f969c647017341b6540ee44789287971ad6a31f
Sultan Alsawaf <sultan@kerneltoast.com>
2019-08-29 18:37:22 -0400

    msm: mdss: Boost DDR bus when committing a new frame
    In order to reduce jank, request a DDR bus boost whenever a new frame is
    ready to be rendered to the display. The boost should be sufficient
    enough to render 60 FPS without any dropped frames when there is no
    significant external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -47,6 +47,7 @@
 #include <linux/sw_sync.h>
 #include <linux/file.h>
 #include <linux/kthread.h>
+#include <linux/devfreq_boost.h>
 #include <linux/dma-buf.h>
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
@@ -4915,6 +4916,9 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+#ifdef CONFIG_DEVFREQ_BOOST
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
