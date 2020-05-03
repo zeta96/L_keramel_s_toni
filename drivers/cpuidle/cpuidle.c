b799a693e45472b13db8f2b415d288c68356ec5c
zeta96 <ahmad.zainul857@gmail.com>
2020-05-03 23:53:50 +0000

    Revert "cpuidle: Optimize pm_qos notifier callback"
    old
    This reverts commit 13c1f7eb5730f5c4bfe688970a5530166bd625cc.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -34,9 +34,6 @@ static int off __read_mostly;
 static int initialized __read_mostly;
 static bool use_deepest_state __read_mostly;
 
-static void cpuidle_set_idle_cpu(unsigned int cpu);
-static void cpuidle_clear_idle_cpu(unsigned int cpu);
-
 int cpuidle_disabled(void)
 {
 	return off;
@@ -128,9 +125,7 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	trace_cpu_idle_rcuidle(index, dev->cpu);
 	time_start = ktime_get();
 
-	cpuidle_set_idle_cpu(dev->cpu);
 	entered_state = target_state->enter(dev, drv, index);
-	cpuidle_clear_idle_cpu(dev->cpu);
 
 	time_end = ktime_get();
 	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, dev->cpu);
@@ -545,20 +540,10 @@ int cpuidle_register(struct cpuidle_driver *drv,
 EXPORT_SYMBOL_GPL(cpuidle_register);
 
 #ifdef CONFIG_SMP
-static atomic_t idle_cpu_mask = ATOMIC_INIT(0);
 
-#if NR_CPUS > 32
-#error idle_cpu_mask not big enough for NR_CPUS
-#endif
-
-static void cpuidle_set_idle_cpu(unsigned int cpu)
+static void smp_callback(void *v)
 {
-	atomic_or(BIT(cpu), &idle_cpu_mask);
-}
-
-static void cpuidle_clear_idle_cpu(unsigned int cpu)
-{
-	atomic_andnot(BIT(cpu), &idle_cpu_mask);
+	/* we already woke the CPU up, nothing more to do */
 }
 
 /*
@@ -570,27 +555,13 @@ static void cpuidle_clear_idle_cpu(unsigned int cpu)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	static unsigned long prev_latency[NR_CPUS] = {
-		[0 ... NR_CPUS - 1] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE
-	};
-	struct cpumask update_mask = CPU_MASK_NONE;
-	unsigned int cpu;
+	const struct cpumask *cpus;
 
-	/* Only send an IPI when the CPU latency requirement is tightened */
-	for_each_cpu(cpu, v) {
-		if (l < prev_latency[cpu])
-			cpumask_set_cpu(cpu, &update_mask);
-		prev_latency[cpu] = l;
-	}
+	cpus = v ?: cpu_online_mask;
 
-	if (!cpumask_empty(&update_mask)) {
-		unsigned long idle_cpus = atomic_read(&idle_cpu_mask);
-
-		cpumask_and(&update_mask, &update_mask, to_cpumask(&idle_cpus));
-
-		/* Notifier is called with preemption disabled */
-		arch_send_call_function_ipi_mask(&update_mask);
-	}
+	preempt_disable();
+	smp_call_function_many(cpus, smp_callback, NULL, 1);
+	preempt_enable();
 
 	return NOTIFY_OK;
 }
